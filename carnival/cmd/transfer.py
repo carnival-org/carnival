2bd83119a57b2dd3cefb749efa1e634b18650f1f
Dmirty Simonov <demalf@gmail.com>
2020-01-28 18:06:29 +0300

    transfer fixes

--- carnival/cmd/transfer.py
+++ carnival/cmd/transfer.py
@@ -2,13 +2,13 @@ from fabric.transfer import Transfer, Result
 from patchwork import files
 from patchwork import transfers
 
-from carnival.global_context import conn
+from carnival import global_context
 
 
 def rsync(source, target, exclude=(), delete=False, strict_host_keys=True, rsync_opts='', ssh_opts=''):
     # https://fabric-patchwork.readthedocs.io/en/latest/api/transfers.html#patchwork.transfers.rsync
     return transfers.rsync(
-        c=conn,
+        c=global_context.conn,
         source=source,
         target=target,
         exclude=exclude,
@@ -21,26 +21,26 @@ def rsync(source, target, exclude=(), delete=False, strict_host_keys=True, rsync
 
 def get(remote: str, local: str, preserve_mode: bool) -> Result:
     # http://docs.fabfile.org/en/2.5/api/transfer.html#fabric.transfer.Transfer.get
-    t = Transfer(conn)
+    t = Transfer(global_context.conn)
     return t.get(remote=remote, local=local, preserve_mode=preserve_mode)
 
 
 def put(local: str, remote: str, preserve_mode: bool) -> Result:
     # http://docs.fabfile.org/en/2.5/api/transfer.html#fabric.transfer.Transfer.put
-    t = Transfer(conn)
+    t = Transfer(global_context.conn)
     return t.put(local=local, remote=remote, preserve_mode=preserve_mode)
 
 
 def is_file_contains(filename, text, exact=False, escape=True):
     # https://fabric-patchwork.readthedocs.io/en/latest/api/files.html#patchwork.files.contains
-    return files.contains(conn, runner=conn.run, filename=filename, text=text, exact=exact, escape=escape)
+    return files.contains(global_context.conn, runner=global_context.conn.run, filename=filename, text=text, exact=exact, escape=escape)
 
 
 def is_file_exists(path) -> bool:
     # https://fabric-patchwork.readthedocs.io/en/latest/api/files.html#patchwork.files.exists
-    return files.exists(conn, runner=conn.run, path=path)
+    return files.exists(global_context.conn, runner=global_context.conn.run, path=path)
 
 
 def is_dir_exists(path, user=None, group=None, mode=None) -> bool:
     # https://fabric-patchwork.readthedocs.io/en/latest/api/files.html#patchwork.files.directory
-    return files.directory(conn, runner=conn.run, path=path, user=user, group=group, mode=mode)
+    return files.directory(global_context.conn, runner=global_context.conn.run, path=path, user=user, group=group, mode=mode)

50ba58bfc80f59d241b855f71eb55d3c9f34145d
Dmirty Simonov <demalf@gmail.com>
2020-01-29 13:26:25 +0300

    new mkdirs command

--- carnival/cmd/cli.py
+++ carnival/cmd/cli.py
@@ -3,3 +3,7 @@ from carnival.core.utils import run_command
 
 def run(command: str, **kwargs):
     return run_command(command, **kwargs)
+
+
+def mkdirs(*dirs: str):
+    return [run(f"mkdir -p {x}") for x in dirs]

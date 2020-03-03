f2b55a376d90957e8041a8fe4daa0ed8cc710c0a
Dmirty Simonov <demalf@gmail.com>
2020-03-03 14:55:09 +0300

    cmd.cli return annotations

--- carnival/cmd/cli.py
+++ carnival/cmd/cli.py
@@ -1,13 +1,15 @@
+from invoke import Result  # type: ignore
+
 from carnival import global_context
 
 
-def _run_command(command: str, **kwargs):
+def _run_command(command: str, **kwargs) -> Result:
     return global_context.conn.run(command, **kwargs)
 
 
-def run(command: str, **kwargs):
+def run(command: str, **kwargs) -> Result:
     return _run_command(command, **kwargs)
 
 
-def pty(command: str, **kwargs):
+def pty(command: str, **kwargs) -> Result:
     return run(command, pty=True, **kwargs)
